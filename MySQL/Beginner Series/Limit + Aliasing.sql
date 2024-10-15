5f9f11c6bf108a9480ce59032285a1782d027629
Fasih Ahmed <fasiha042@gmail.com>
2024-10-15 16:38:29 +0500

    Add files via upload

---
+++ MySQL/Beginner Series/Limit + Aliasing.sql
@@ -0,0 +1,22 @@
+-- LIMIT & ALIASING
+
+SELECT *
+FROM parks_and_recreation.employee_demographics
+ORDER BY age DESC
+LIMIT 3;
+
+SELECT *
+FROM parks_and_recreation.employee_demographics
+ORDER BY age DESC
+LIMIT 2, 1;
+
+-- ALIASING
+SELECT gender, AVG(age) AS avg_age
+FROM parks_and_recreation.employee_demographics
+GROUP BY gender
+HAVING avg_age > 40;
+
+SELECT gender, AVG(age) avg_age
+FROM parks_and_recreation.employee_demographics
+GROUP BY gender
+HAVING avg_age > 40;
